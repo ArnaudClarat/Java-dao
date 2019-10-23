8e415984736dce13fe27fcc13ee12e91866925aa
Arnaud Clarat <arnaud.clarat@live.fr>
2019-10-23 21:31:43 +0200

    Fin Cours 23/10

---
+++ src/main/java/poo/dao/dao/DAO.java
@@ -0,0 +1,15 @@
+package poo.dao.dao;
+
+import java.util.Collection;
+
+public interface DAO<T> {
+	T find(long id); //READ
+	
+	T save(T object); //CREATE
+	
+	T update(T object); //UPDATE
+	
+	boolean delete(T object); //DELETE
+	
+	void saveAll(Collection<T> collection); //SAVE ALL
+}
